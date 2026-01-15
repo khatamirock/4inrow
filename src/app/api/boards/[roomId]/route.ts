fa6f6d0e2fdb5d0c79d0f818e5c426e930f9a3b3
khatamirock <khatamionik@gmail.com>
2026-01-15 19:00:58 +0600

    !!

--- src/app/api/boards/[roomId]/route.ts
+++ src/app/api/boards/[roomId]/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { get } from "@vercel/edge-config";
 
 export async function GET(
-  req: NextRequest,
   { params }: { params: Promise<{ roomId: string }> }
 ) {
   try {
